b862d7b3bcadca85d9f389201e8ca6d634c61f17
smasiner <m.shaihutdinov@innopolis.university>
2021-09-21 22:41:29 +0300

    fifth lab commit

---
+++ week5/ex1WithFixing.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+struct threadInfo{
+    pthread_t id;
+    int threadNumberCreated;
+    char* stringToPronounceTemplate;
+};
+
+void *job(void *argc){
+    struct threadInfo temp = *((struct threadInfo*) argc);
+    temp.id = pthread_self();
+    printf("%s", temp.stringToPronounceTemplate);
+    printf("%d", temp.threadNumberCreated);
+    printf(", It has the id: %ld \n", (long )temp.id);
+    return NULL;
+}
+int main(){
+    int N;
+    printf("N = ");
+    scanf("%d", &N);
+    printf("\n");
+    struct threadInfo *tInfo = calloc(N, sizeof (struct threadInfo));
+    for (int i = 0; i < N; i++){
+        tInfo[i].threadNumberCreated = i;
+        tInfo[i].stringToPronounceTemplate = "Hello from thread number: ";
+        pthread_create(&tInfo[i].id, NULL, job, &tInfo[i]);
+        printf("Thread number %d has been created\n", i);
+        pthread_join(tInfo[i].id, NULL); //fixing
+    }
+	pthread_exit(NULL);
+    return 0;
+}
---
+++ week5/ex2.sh
@@ -0,0 +1,9 @@
+while [ $(tail -1 f.txt ) -le 100 ]; # Althogh it obvious by task, be sure that f.txt contains at least one number.
+  do
+  if ln f.txt f.lock # locking the file
+  then
+    var=$(tail -1 f.txt | grep -oE '^[0-9]+$') # greap to find last number
+    echo $((var + 1)) >> f.txt # add last number + 1 at the end
+    rm f.lock # unlocking the file
+    fi
+  done
