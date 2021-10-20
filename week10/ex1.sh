3fa0e3a892371ee72261acf7f454a1c42a09a24e
smasiner <m.shaihutdinov@innopolis.university>
2021-10-20 17:13:15 +0300

    lab 10 commit(onlab)

---
+++ week10/ex1.sh
@@ -0,0 +1,8 @@
+touch _ex1.txt
+echo 'Shaikhutdinov Murat Robertovich' > _ex1.txt
+ln _ex1.txt _ex1_1.txt
+ln _ex1.txt _ex1_2.txt
+ls -i _ex1.txt >> ex1.txt
+ls -i _ex1_1.txt >> ex1.txt
+ls -i _ex1_2.txt >> ex1.txt
+
