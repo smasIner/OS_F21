ad327cd7b16650cbcccdbf4b71c4806847d40bf1
smasiner <m.shaihutdinov@innopolis.university>
2021-10-26 10:02:32 +0300

    lab10

---
+++ week10/ex3.sh
@@ -0,0 +1,12 @@
+touch _ex3.txt
+ls -l _ex3.txt
+chmod u=rw _ex3.txt >> ex3.txt
+chmod g=r _ex3.txt >> ex3.txt
+chmod o=r _ex3.txt >> ex3.txt
+ls -l _ex3.txt
+chmod u=rwx _ex3.txt >> ex3.txt
+chmod o=rwx _ex3.txt >> ex3.txt
+ls -l _ex3.txt
+chmod g=rwx _ex3.txt >> ex3.txt
+ls -l _ex3.txt
+nano ex3.txt
